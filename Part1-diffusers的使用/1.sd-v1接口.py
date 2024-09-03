8af84701ccd029898ec8694852b6aee254f8a4f3
TuobaZhen <2459769486@qq.com>
2024-09-03 10:25:58 +0800

    完成基础API

--- Part1-diffusers的使用/1.sd-v1接口.py
+++ Part1-diffusers的使用/1.sd-v1接口.py
@@ -1,15 +1,17 @@
+import os
 import torch
 import matplotlib.pyplot as plt
 from diffusers import DiffusionPipeline
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+device='cuda' if torch.cuda.is_available() else "cpu"
+    
 print(device)
 
 pipeline = DiffusionPipeline.from_pretrained("/data_disk/dyy/models").to(device)
 #查看/data_disk/dyy/models是否存在
-image=pipeline(prompt="a and nylon stockings,European girl", num_inference_steps=50).images[0]
+image=pipeline(prompt="a and nylon stockings ,bra ", num_inference_steps=50).images[0]
+
 print(type(image))
 #将这片图片显示出来
 plt.imshow(image)
@@ -17,3 +19,23 @@ plt.axis('off')
 plt.show()
 
 print(pipeline)
+
+
+
+pipeline=DiffusionPipeline.from_pretrained("/data_disk/dyy/models").to(device)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- try.py
+++ try.py
@@ -1,27 +1,9 @@
-import torch
-print(torch.__version__)
-import matplotlib.pyplot as plt
-print(3)
-
-if torch.cuda.is_available():
-    print('yes')
-else :
-    print('no')
 
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-# 创建一些数据
-x = np.linspace(0, 5, 100)
-y = np.sin(x)
-
-# 创建图像
-plt.plot(x, y)
-plt.title('Sine Wave')
-
-# 保存图像到当前目录
-plt.savefig('./UNet-based_diffusion.png')
-
-# 显示图像（可选）
-plt.show()
+def plus(a):
+    for i in range(10):
+        a.append(i)
+a=[]
+if __name__ == '__main__':
+    plus(a)
+    print(a)
